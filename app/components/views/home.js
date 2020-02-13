a9155f42413200e643a0a3242210ebedb0ec8b15
christopher watts <chris@byhook.com>
2020-02-13 13:33:03 -0800

    rigging up ant components

--- app/components/views/home.js
+++ app/components/views/home.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { Row, Col } from 'antd';
 const Home = props => (
-    <div>
-        Home 2
-    </div>
+
+       <div>
+         <h1>Standard Banner Minifier</h1>
+         <div>
+           <Row>
+             <Col span={12}>col-12</Col>
+             <Col span={12}>col-12</Col>
+           </Row>
+         </div>
+       </div>
+
 );
 
 export default Home;
